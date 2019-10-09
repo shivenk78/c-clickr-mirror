3e116c58edcc400a05b6299de9ff2c1fa183182d
asd6 <asd6@illinois.edu>
2019-10-08 22:49:00 -0500

    Updating test code; now allows user input patterns, random patterns, and continuous random patterns; DO NOT TEST THIS ON SUBLIME

--- RandomPattern.py
+++ RandomPattern.py
@@ -1,40 +1,76 @@
 import random
 import time
-from tkinter import *
+import tkinter as tk
 
-master = Tk()
+master = tk.Tk()
 
-#set dimensions of canvas
-canvas = Canvas(master, width=1000, height=1000)
+#sets dimensions of canvas
+canvas = tk.Canvas(master, width=1000, height=1000)
 canvas.pack()
 
-#set dimensions of each quadrant
+#sets dimensions of each quadrant
 xDim = 100
 yDim = xDim*2
 
-#assign each digit in code to a color
+#assigns each digit in code to a color 
 colorDict = {0: "red", 1: "blue", 2: "yellow", 3: "green"}
 
-def generatePattern() :
+#generates pattern with a given code
+def generatePattern(newCode) :
 	canvas.delete("all")
+
+	#fills each quadrant with its respective color
+	canvas.create_rectangle(0, 0, xDim, yDim, fill=colorDict[newCode[0]])
+	canvas.create_rectangle(xDim, 0, xDim*2, yDim, fill=colorDict[newCode[1]])
+	canvas.create_rectangle(0, yDim, xDim, yDim*2, fill=colorDict[newCode[2]])
+	canvas.create_rectangle(xDim, yDim, xDim*2, yDim*2, fill=colorDict[newCode[3]])
+	canvas.create_rectangle(0, yDim*2, xDim*2, yDim*2+yDim*(1/4), fill="purple")
+
+	#sets title of window as 4-digit code
+	newStr = ""
+	for i in newCode:
+		newStr += str(i)
+	master.title(newStr)
+
+	master.update()
+
+#generates pattern based on user input
+def generateInputPattern() :
+        inputStr = input("Enter a 4-digit code XXXX using [0,1,2,3]: ")
+        inputCode = [int(inputStr[0]),int(inputStr[1]),int(inputStr[2]),int(inputStr[3])]
+        print(inputCode)
+        
+        generatePattern(inputCode)
+
+#generates random pattern
+def generateRandomPattern() :
 	#generate 4-digit random code using all of 0,1,2,3
 	code = [];
 	while (len(code) < 4):
 		i = random.randrange(0, 4)
-		print(i)
+		#print(i)
 		if not (i in code):
 			code.append(i)
 	print(code)
-	
-	#fill each quadrant with its respective color
-	canvas.create_rectangle(0, 0, xDim, yDim, fill=colorDict[code[0]])
-	canvas.create_rectangle(xDim, 0, xDim*2, yDim, fill=colorDict[code[1]])
-	canvas.create_rectangle(0, yDim, xDim, yDim*2, fill=colorDict[code[2]])
-	canvas.create_rectangle(xDim, yDim, xDim*2, yDim*2, fill=colorDict[code[3]])
-	canvas.create_rectangle(0, yDim*2, xDim*2, yDim*2+yDim*(1/4), fill="purple")
 
-	master.update()
-	master.after(500, generatePattern)	
+	generatePattern(code)
 
-master.after(0, generatePattern)
-mainloop()
+#generates random patterns in fixed time intervals
+def generateContinuousRandomPattern() :
+	generateRandomPattern()
+	timeDiff = 500
+	master.after(timeDiff, generateContinuousRandomPattern)  
+
+mode = input("Select mode: input (I), random (R), continuous random (CR): ")
+chooseMode = {
+        "I": generateInputPattern,
+        "R": generateRandomPattern,
+        "CR": generateContinuousRandomPattern
+}
+try:
+        func = chooseMode.get(mode)
+        func()
+except:
+        print("Invalid mode")
+
+tk.mainloop()
