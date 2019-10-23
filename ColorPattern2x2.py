fd0ce65adf9a479e9c572c95c2b19cf401502d49
asd6 <asd6@illinois.edu>
2019-10-23 10:08:20 -0500

    Cleaned up some code

--- ColorPattern2x2.py
+++ ColorPattern2x2.py
@@ -9,47 +9,43 @@ canvas = tk.Canvas(master, width=1000, height=1000)
 canvas.pack()
 
 #sets dimensions of each quadrant
-xDim = 100
+xDim = 150
 yDim = xDim*2
 
 #assigns each digit in code to a color 
 colorDict = {0: "red", 1: "blue", 2: "yellow", 3: "green"}
 
 #generates pattern with a given code
-def generatePattern(newCode) :
+def generatePattern(newCodeStr) :
+	print(newCodeStr)
 	canvas.delete("all")
 
+	code = [int(newCodeStr[0]),int(newCodeStr[1]),int(newCodeStr[2]),int(newCodeStr[3])]
+
 	#fills each quadrant with its respective color
-	canvas.create_rectangle(0, 0, xDim, yDim, fill=colorDict[newCode[0]])
-	canvas.create_rectangle(xDim, 0, xDim*2, yDim, fill=colorDict[newCode[1]])
-	canvas.create_rectangle(0, yDim, xDim, yDim*2, fill=colorDict[newCode[2]])
-	canvas.create_rectangle(xDim, yDim, xDim*2, yDim*2, fill=colorDict[newCode[3]])
+	canvas.create_rectangle(0, 0, xDim, yDim, fill=colorDict[code[0]])
+	canvas.create_rectangle(xDim, 0, xDim*2, yDim, fill=colorDict[code[1]])
+	canvas.create_rectangle(0, yDim, xDim, yDim*2, fill=colorDict[code[2]])
+	canvas.create_rectangle(xDim, yDim, xDim*2, yDim*2, fill=colorDict[code[3]])
 	canvas.create_rectangle(0, yDim*2, xDim*2, yDim*2+yDim*(1/4), fill="purple")
 
 	#sets title of window as 4-digit code
-	newStr = ""
-	for i in newCode:
-		newStr += str(i)
-	master.title(newStr)
-
+	master.title(newCodeStr)
 	master.update()
 
 #generates pattern based on user input
 def generateInputPattern() :
-        inputStr = input("Enter a 4-digit code XXXX using [0,1,2,3]: ")
-        inputCode = [int(inputStr[0]),int(inputStr[1]),int(inputStr[2]),int(inputStr[3])]
-        print(inputCode)
-        
-        generatePattern(inputCode)
+    inputStr = input("Enter a 4-digit code XXXX using [0,1,2,3]: ")
+
+    generatePattern(inputStr)
 
 #generates random pattern
 def generateRandomPattern() :
-	code = [];
-	while (len(code) < 4):
-		code.append(random.randrange(0, 4))
-	print(code)
+	codeStr = ""
+	while (len(codeStr) < 4):
+		codeStr += str(random.randrange(0, 4))
 
-	generatePattern(code)
+	generatePattern(codeStr)
 
 #generates random patterns in fixed time intervals
 def generateContinuousRandomPattern() :
@@ -57,11 +53,11 @@ def generateContinuousRandomPattern() :
 	timeDiff = 500
 	master.after(timeDiff, generateContinuousRandomPattern)  
 
-mode = input("Select mode: input (I), random (R), continuous random (CR): ")
+mode = input("Select mode: input (i), random (r), continuous random (c): ")
 chooseMode = {
-        "I": generateInputPattern,
-        "R": generateRandomPattern,
-        "CR": generateContinuousRandomPattern
+        "i": generateInputPattern,
+        "r": generateRandomPattern,
+        "c": generateContinuousRandomPattern
 }
 try:
         func = chooseMode.get(mode)
