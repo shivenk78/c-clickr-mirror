062243c042be9bbf331d0a27b958c054fa79ff1e
shivenk78 <kumar.shiven15@gmail.com>
2019-10-22 19:06:31 -0500

    pink blue red green color masking

--- colorDetect/colorDetectV1.py
+++ colorDetect/colorDetectV1.py
@@ -10,14 +10,33 @@ while(1):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     lower_pink = np.array([130,50,70])
     upper_pink = np.array([150,255,255])
-  
+
+    lower_blue = np.array([100,50,50])
+    upper_blue = np.array([125,255,255])
+
+    lower_red = np.array([160,50,50])
+    upper_red = np.array([190,255,255])
+
+    lower_green = np.array([70,50,50])
+    upper_green = np.array([100,255,255])
+
+    lower_yellow = np.array([70,50,50])
+    upper_yellow = np.array([100,255,255])
+
     # Define color range
-    mask = cv2.inRange(hsv, lower_pink, upper_pink) 
+    pink_mask = cv2.inRange(hsv, lower_pink, upper_pink) 
+    blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    red_mask = cv2.inRange(hsv, lower_red, upper_red)
+    green_mask = cv2.inRange(hsv, lower_green, upper_green)
+
+    super_mask = cv2.bitwise_or(pink_mask, blue_mask)
+    super_mask = cv2.bitwise_or(super_mask, red_mask)
+    super_mask = cv2.bitwise_or(super_mask, green_mask)
     
     # Color Masking
-    res = cv2.bitwise_and(frame,frame, mask= mask) 
+    res = cv2.bitwise_and(frame,frame, mask= super_mask) 
     cv2.imshow('frame',frame) 
-    cv2.imshow('mask',mask) 
+    cv2.imshow('mask',super_mask) 
     cv2.imshow('res',res) 
     
     # Shows the 3 different camera views in separate windows
@@ -25,8 +44,6 @@ while(1):
     if k == 27: 
         break
   
-# Destroys all of the HighGUI windows. 
+# Destroys all of the HighGUI windows, then releases the frame
 cv2.destroyAllWindows() 
-  
-# release the captured frame 
 cap.release() 
