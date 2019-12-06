0497f0e0b70dca1083fb2f9f31249c18c653f4f6
Maxwell Wang <mlwang3@illinois.edu>
2019-12-05 22:47:04 -0800

    contour finding works with the pattern detection

--- Color Coordinate Detector/colorcoordinates.py
+++ Color Coordinate Detector/colorcoordinates.py
@@ -10,6 +10,7 @@ import cv2
 import numpy as np
 import math
 import pyscreenshot as imageGrab
+import imutils
 
 
 # create class to store pattern objects
@@ -28,9 +29,9 @@ class coordinates:
 
 
 # capturing video through webcam
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0)
-cap.set(cv2.CAP_PROP_AUTO_WB, 0)
+# cap = cv2.VideoCapture(0)
+# cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0)
+# cap.set(cv2.CAP_PROP_AUTO_WB, 0)
 
 # list of distances found
 patternList = []
@@ -50,6 +51,7 @@ while (1):
     blue = img[:, :, 0].copy()
     img[:, :, 0] = red
     img[:, :, 2] = blue
+
     img = cv2.bilateralFilter(img, 9, 75, 75)
 
     # list is cleared for each run through
@@ -133,33 +135,48 @@ while (1):
                         patternList.append(p1)
                         idCount = idCount + 1
     # show each distance calculated
-    crop_img = img
+    crop_img = img.copy()
     for thing in patternList:
+        print("detected")
         # cv2.line(img, (thing.top.x, thing.top.y), (thing.bottom.x, thing.bottom.y), (0,0,0), 5)
         if thing.top.x < thing.bottom.x:
-            dist = (thing.bottom.x - thing.top.x) / 2
+            dist = int((thing.bottom.x - thing.top.x) / 2)
             crop_img = img[abs(thing.top.y - dist):thing.top.y + dist, thing.top.x:thing.bottom.x]
         else:
-            dist = (thing.top.x - thing.bottom.x) / 2
+            dist = int((thing.top.x - thing.bottom.x) / 2)
             crop_img = img[abs(thing.top.y - dist):thing.top.y + dist, thing.bottom.x:thing.top.x]
 
-        gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-        # cv2.imshow("gray", gray)
 
-        blur = cv2.GaussianBlur(gray, (5, 5), 0)
-        # cv2.imshow("blur", blur)
+        # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # # cv2.imshow("gray", gray)
+        #
+        # blur = cv2.GaussianBlur(gray, (5, 5), 0)
+        # # cv2.imshow("blur", blur)
+        #
+        # thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
+        #
+        # (contours, ok1) = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
 
-        (contours, ok1) = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (5, 5), 0)
+        edged = cv2.Canny(gray, 75, 200)
+        cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
 
-        c = 0
-        for i in contours:
-            area = cv2.contourArea(i)
-            if area > 1000 / 2:
-                cv2.drawContours(crop_img, contours, c, (0, 255, 0), 3)
-            c += 1
-        cv2.imshow("cropped", crop_img)
+
+        for i in cnts:
+            peri = cv2.arcLength(i, True)
+            approx = cv2.approxPolyDP(i, 0.02 * peri, True)
+            #area = cv2.contourArea(i)
+            # if area > 1000 / 2:
+            #     cv2.drawContours(img, contours, c, (0, 255, 0), 3)
+            if len(approx) == 4:
+                squareContours = approx
+
+        cv2.drawContours(img, [squareContours], -1, (0, 255, 0), 2)
+        cv2.imshow("cropped", img)
 
         # cv2.imshow("cropped", thresh)
         # print("(" + str(thing.top.x) + "," + str(thing.top.y) + ")\t" + "(" + str(thing.bottom.x) + "," + str(thing.bottom.y) + ")\t" + "Distance:" + str(thing.distance))
@@ -167,8 +184,71 @@ while (1):
     # cv2.imshow("Redcolour",red)
     cv2.imshow("Color Tracking", img)
     img = cv2.flip(img, 1)
-    # cv2.imshow("red",res)
+    # cv2.imshow("red",res)qqq
     if cv2.waitKey(10) & 0xFF == ord('q'):
-        cap.release()
+        #cap.release()
         cv2.destroyAllWindows()
         break
+
+
+def four_point_transform(image, pts):
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    # return the warped image
+    return warped
+
+
+def order_points(pts):
+    # initialzie a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype="float32")
+
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+
+    # return the ordered coordinates
+    return rect
