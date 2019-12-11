31703089e52590ae7b48504934b14b3deea3fd83
Angela Luo <ael49021@gmail.com>
2019-12-11 03:23:56 -0600

    rectangle bounds code

---
+++ Finds Bounding Rectangle of Patterns/detect_shapes.py
@@ -0,0 +1,65 @@
+# USAGE
+# python detect_shapes.py --image orange.png
+
+# import the necessary packages
+from shapedetector import ShapeDetector
+import argparse
+import imutils
+import cv2
+
+while(1):
+	# construct the argument parse and parse the arguments
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-i", "--image", required=True,
+		help="path to the input image")
+	args = vars(ap.parse_args())
+
+	# load the image and resize it to a smaller factor so that
+	# the shapes can be approximated better
+	image = cv2.imread(args["image"])
+	resized = imutils.resize(image, width=300)
+	ratio = image.shape[0] / float(resized.shape[0])
+
+	# convert the resized image to grayscale, blur it slightly,
+	# and threshold it
+	gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+	blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+	thresh1 = cv2.threshold(blurred, 90, 255, cv2.THRESH_BINARY)[1]
+	thresh = cv2.threshold(blurred, 160, 220, cv2.THRESH_BINARY)[1]
+	cv2.imshow("thresh", thresh)
+	cv2.imshow("thresh1", thresh1)
+
+	# find contours in the thresholded image and initialize the
+	# shape detector
+	cnts = cv2.findContours(thresh.copy(), cv2.RETR_TREE,
+		cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
+	sd = ShapeDetector()
+
+	maxArea = 0
+	# loop over the contours
+	for c in cnts:
+		# compute the center of the contour, then detect the name of the
+		# shape using only the contour
+		M = cv2.moments(c)
+		cX = int((M["m10"] / M["m00"]) * ratio)
+		cY = int((M["m01"] / M["m00"]) * ratio)
+		shape = sd.detect(c)
+
+		# multiply the contour (x, y)-coordinates by the resize ratio,
+		# then draw the contours and the name of the shape on the image
+		c = c.astype("float")
+		c *= ratio
+		c = c.astype("int")
+		area = cv2.contourArea(c)
+		if (area > maxArea and shape == "rectangle"):
+			maxArea = area
+			cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+			cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+				0.5, (255, 255, 255), 2)
+		# show the output image
+		cv2.imshow("Image", image)
+		if cv2.waitKey(10) & 0xFF == ord('q'):
+					cap.release()
+					cv2.destroyAllWindows()
+					break
